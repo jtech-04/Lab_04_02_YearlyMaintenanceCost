ee0e0bcfc783d6f21f72d097bb1c5d55d50ec75e
jamie <jamiek0414@gmail.com>
2023-09-15 15:50:54 -0400

    Final!!!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int fallCost = 150;
+        int winterCost = 200;
+        int springCost = 250;
+        int summerCost = 300;
+        int homeMaintenance = fallCost + winterCost + springCost + summerCost;
+
+        System.out.println("Your annual home maintenance cost is " + homeMaintenance + " dollars");
     }
 }
